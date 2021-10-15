568fda33afd3d513194585d9809a8122811cfa4b
absandell <absandell@gmail.com>
2021-10-14 22:10:26 -0400

    Testing and Bugfixes with Decryption

--- decipher.py
+++ decipher.py
@@ -5,50 +5,61 @@ from Crypto.Util.Padding import unpad
 from Crypto.Util.py3compat import is_string
 from sys import getsizeof
 
-with open("keygame.txt") as f:
-    lines = f.readlines()
+lines = open("keygame.txt").read().split('\n')
 
 counter = 0
 iv = k0 = k1 = k2 = k3 = k4 = k5 = k6 = k7 = k8 = k9 = k10 = k11 = k12 = k13 = k14 = k15 = ''
 
+
 for line in lines:
     if (counter == 0):
-        iv = line.encode("utf-8")
+        iv = line
         counter += 1
     elif (counter == 1):
-        k0 = line.encode("utf-8")
+        k0 = str.encode(line)
         counter += 1
     elif (counter == 2):
-        k1 = line.encode("utf-8")
+        k1 = str.encode(line)
         counter += 1
     elif (counter == 3):
-        k2 = line.encode("utf-8")
+        k2 = str.encode(line)
         counter += 1
     elif (counter == 4):
-        k3 = line.encode("utf-8")
+        k3 = str.encode(line)
         counter += 1
     elif (counter == 5):
-        k4 = line.encode("utf-8")
+        k4 = str.encode(line)
         counter += 1
     elif (counter == 6):
-        k5 = line.encode("utf-8")
+        k5 = str.encode(line)
         counter += 1
     elif (counter == 7):
-        k6 = line.encode("utf-8")
+        k6 = str.encode(line)
         counter += 1
     elif (counter == 8):
-        k7 = line.encode("utf-8")
+        k7 = str.encode(line)
         counter += 1
-print(len(k0))
-print(len(k1))
+
+print(k0)
+for i in range(len(k0)):
+    print(i+1, ": ", k0[i])
+
+print(k1)
+for i in range(len(k1)):
+    print(i+1, ": ", k1[i])
+
+print(iv)
+for i in range(len(iv)):
+    print(i+1, ": ", iv[i])
 #try:
-iv = b64decode(iv)
-ct = b64decode(k0)
+#iv = b64decode(iv)
+#ct = b64decode(iv)
+
 cipher = AES.new(k0, AES.MODE_CBC, iv)
-data = unpad(cipher.decrypt(ct), AES.block_size)
+data = unpad(cipher.decrypt(k0), AES.block_size)
 print("The message was: ", data)
 #except ValueError:
-  #  print("Incorrect decryption")
+#  print("Incorrect decryption")
 
 #iv = b64decode(b64['iv'])
 #ct = b64decode(b64['ciphertext'])
