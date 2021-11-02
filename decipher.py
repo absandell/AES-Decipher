16b085121ac2f0bf1cdd5bf3b03b4cf5bced487e
absandell <absandell@gmail.com>
2021-11-02 09:45:26 -0400

    Reconfigure decoder

--- decipher.py
+++ decipher.py
@@ -1,67 +1,32 @@
-import json
-from base64 import b64decode
+import base64
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import unpad
-from Crypto.Util.py3compat import is_string
-from sys import getsizeof
 
-lines = open("keygame.txt").read().split('\n')
+def xor_strings(a,b):
+    return "".join([chr(ord(x)^ord(y)) for x,y in zip(a,b)])
 
-counter = 0
-iv = k0 = k1 = k2 = k3 = k4 = k5 = k6 = k7 = k8 = k9 = k10 = k11 = k12 = k13 = k14 = k15 = ''
+lines = open("keygame.txt", 'r')
+keys = lines.read().split("\n")
+lines.close()
 
+key0 = keys[0]
+cipher_text = key0.encode("utf-8")
 
-for line in lines:
-    if (counter == 0):
-        iv = line
-        counter += 1
-    elif (counter == 1):
-        k0 = str.encode(line)
-        counter += 1
-    elif (counter == 2):
-        k1 = str.encode(line)
-        counter += 1
-    elif (counter == 3):
-        k2 = str.encode(line)
-        counter += 1
-    elif (counter == 4):
-        k3 = str.encode(line)
-        counter += 1
-    elif (counter == 5):
-        k4 = str.encode(line)
-        counter += 1
-    elif (counter == 6):
-        k5 = str.encode(line)
-        counter += 1
-    elif (counter == 7):
-        k6 = str.encode(line)
-        counter += 1
-    elif (counter == 8):
-        k7 = str.encode(line)
-        counter += 1
+key9 = keys[9]
+g = "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG"
+print (len(key9))
 
-print(k0)
-for i in range(len(k0)):
-    print(i+1, ": ", k0[i])
+print(len(g))
+key9 = xor_strings(key9[7:], g)
+keys[9] = 'KEY009-'+key9
+print(keys[9])
 
-print(k1)
-for i in range(len(k1)):
-    print(i+1, ": ", k1[i])
+iv = b"\xb5\xcdt\xe0\x91\xdb8V\x10\xbf\xab;'\xd8\xe9\x8a"
+decrypt_cipher = AES.new(cipher_text, AES.MODE_CBC, iv)
 
-print(iv)
-for i in range(len(iv)):
-    print(i+1, ": ", iv[i])
-#try:
-#iv = b64decode(iv)
-#ct = b64decode(iv)
+for k in keys[1:]:
+    if k != "":
+        cipher_text += base64.b64decode(k[7:])
 
-cipher = AES.new(k0, AES.MODE_CBC, iv)
-data = unpad(cipher.decrypt(k0), AES.block_size)
-print("The message was: ", data)
-#except ValueError:
-#  print("Incorrect decryption")
+plain_text = decrypt_cipher.decrypt(cipher_text)
 
-#iv = b64decode(b64['iv'])
-#ct = b64decode(b64['ciphertext'])
-#cipher = AES.new(key, AES.MODE_CBC, iv)
-#pt = unpad(cipher.decrypt(ct), AES.block_size)
+print(plain_text.decode())
